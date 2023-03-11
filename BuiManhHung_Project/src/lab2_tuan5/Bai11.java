f925c429a199efc7c22c8d8c68798cea60db61c0
2121051453 <125479080+2121051453@users.noreply.github.com>
2023-03-11 20:30:38 +0700

    firt commit

---
+++ BuiManhHung_Project/src/lab2_tuan5/Bai11.java
@@ -0,0 +1,31 @@
+package lab2_tuan5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bai11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Nhập số phần tử của mảng: ");
+        int n = scanner.nextInt();
+        
+        
+        int[] a = new int[n];
+        
+        
+        System.out.println("Nhập giá trị cho các phần tử trong mảng:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("a[" + i + "] = ");
+            a[i] = scanner.nextInt();
+        }
+        
+        
+        Arrays.sort(a);
+        
+        
+        System.out.println("Mảng số ban đầu: " + Arrays.toString(a));
+        System.out.println("Mảng sắp xếp theo thứ tự tăng dần: " + Arrays.toString(a));
+    }
+}
